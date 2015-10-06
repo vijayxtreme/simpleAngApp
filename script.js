df07b947c0b1a35ee3ec28ecfd6e38a002efd5dd
Equate <Equate@Admins-Mac-mini.local>
2015-10-06 08:53:38 -0700

    Add Resource, Routes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //declare a global myapp variable that gets the angular module and its dependencies
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngResource', 'ngRoute']);
 
 //declare a controller -- controls the view 
 myApp.controller('mainController', ['$scope', function($scope){
